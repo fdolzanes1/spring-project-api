9b45597520f1f0121f267b51e71364e60f91f11a
FabianoDolzanes <fdolzanes1@gmail.com>
2023-02-03 14:01:04 -0400

    first commit

---
+++ src/main/java/com/dolzanes/project/SpringProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.dolzanes.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApiApplication.class, args);
+	}
+
+}
